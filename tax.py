dfecc27c531b10f9491bf660b3f8e289121c87da
kaungsatkyaw <fired6559@gmail.com>
2019-10-27 18:16:55 +0630

    tax cal

---
+++ tax.py
@@ -0,0 +1,16 @@
+#Sample
+charge = 10000
+tax = 50 / 1000
+totaltax = charge * tax
+totalpaid = totaltax + charge
+totalpaid = ?
+
+#problem
+ charge = 10000
+ tax = 50 / 1000
+ totaltax = charge * tax
+ totaltax
+ 500.0
+ totalpaid = totaltax + charge
+ totalpaid
+ 10500.0
